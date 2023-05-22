f4fef0d3c05058e59a7ab9fb35f5fc7976902bc1
Murat Göğebakan <36950001+MuratCaganGogebakan@users.noreply.github.com>
2023-05-22 12:55:54 +0300

    recommend multiple games

--- gradio_app.py
+++ gradio_app.py
@@ -26,24 +26,30 @@ docsearch = Pinecone.from_existing_index(index_name=index_name, embedding=embedd
 llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
 chain = load_qa_chain(llm, chain_type="stuff")
 
-def reccommend_game(query, game_name):
+def recommend_game(query, game_name):
     game_docs = docsearch.similarity_search(query, k=5, filter={"game": game_name})
     query = "Reccommend this game to me, If you wouldn't reccommend it always start your answer wtih a No. Make relations to my query: " + query
     answer = chain.run(input_documents=game_docs, question=query)
     return answer
 
-def reccomend_games(query, k=5):
+def recommend_games(query, k=5):
     docs = docsearch.similarity_search_with_score(query, k)
     games = []
     for i in range(len(docs)):
         games.append(docs[i][0].metadata["game"][0])
-    return reccommend_game(query, games[0])
+    # Make the games unique
+    games = list(set(games))
+    recommendations = []
+    for game in games:
+        recommendations.append(recommend_game(query, game))
+    formatted_recommendations = [f"## {i+1}. {games[i]}\n\n{recommendation}" for i, recommendation in enumerate(recommendations)]  # add index, markdown formatting and new lines
+    return "\n".join(formatted_recommendations)
 
 iface2 = gr.Interface(
-    fn=reccomend_games,
+    fn=recommend_games,
     inputs=[
         gr.inputs.Textbox(lines=2, placeholder="Describe a game you would like", label="Game Description"),
     ],
-    outputs="text"
+    outputs="markdown"
 )
 iface2.launch()
